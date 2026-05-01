a798d8e3db52e814f283e060b17e91608c5a09f9
˹ 🎧 ᴛᴇᴀᴍ ɪɴғɪɴɪᴛʏ ʙᴏᴛs ˼ <networksmite@gmail.com>
2026-05-01 23:44:24 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- NOBITA_MUSIC/utils/inline/play.py
+++ NOBITA_MUSIC/utils/inline/play.py
@@ -40,25 +40,25 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "💿—————————"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "—💗————————"
     elif 20 <= umm < 30:
-        bar = "——𝚴———————"
+        bar = "——🎶———————"
     elif 30 <= umm < 40:
-        bar = "———𝐎——————"
+        bar = "———🥀——————"
     elif 40 <= umm < 50:
-        bar = "————𝐁—————"
+        bar = "————💘—————"
     elif 50 <= umm < 60:
-        bar = "—————𝚰————"
+        bar = "—————❄️————"
     elif 60 <= umm < 70:
-        bar = "——————𝐓———"
+        bar = "——————🔍———"
     elif 70 <= umm < 80:
-        bar = "———————𝚲——"
+        bar = "———————⛩️——"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "————————😭—"
     else:
-        bar = "—————————◉"
+        bar = "—————————🗑️"
 
     buttons = [
         [
@@ -76,10 +76,10 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="𝖮ᴡɴᴇʀ 🥀", url="https://t.me/ll_NOBITA_DEFAULTERS_ll",
+                text="𝖮ᴡɴᴇʀ 🥀", url="https://t.me/gandhiji_yoyo",
             ),
             InlineKeyboardButton(
-                text="𝖲ᴜᴘᴘᴏʀᴛ 🥀", url="https://t.me/+WLTHgUAvkYVmNTg9",
+                text="𝖲ᴜᴘᴘᴏʀᴛ 🥀", url="https://t.me/theinfinity_support",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
