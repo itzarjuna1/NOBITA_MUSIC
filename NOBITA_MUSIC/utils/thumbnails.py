cc8459cff34ced2318dde02c6d18404b387f5ffc
˹ 🎧 ᴛᴇᴀᴍ ɪɴғɪɴɪᴛʏ ʙᴏᴛs ˼ <networksmite@gmail.com>
2026-05-05 10:52:38 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- NOBITA_MUSIC/utils/thumbnails.py
+++ NOBITA_MUSIC/utils/thumbnails.py
@@ -1,9 +1,12 @@
+import random
 from config import YOUTUBE_IMG_URL
 
 
 async def get_thumb(videoid: str):
     """
-    Returns static thumbnail from config.
-    No processing, no caching, no API calls.
+    Returns a random thumbnail from config list.
     """
+    if isinstance(YOUTUBE_IMG_URL, list):
+        return random.choice(YOUTUBE_IMG_URL)
     return YOUTUBE_IMG_URL
+    
