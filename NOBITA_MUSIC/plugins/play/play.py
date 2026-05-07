3008a06e14f06a1322d9ea5c4a4000a69d406f5d
ARJUN <pachangedinanath@gmail.com>
2026-05-07 10:37:37 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- NOBITA_MUSIC/plugins/play/play.py
+++ NOBITA_MUSIC/plugins/play/play.py
@@ -57,7 +57,7 @@ async def play_commnd(
 ):
     
     mystic = await message.reply_text(
-    photo="https://graph.org/file/8845649629c4d7025c37f-06bc4b7900f7f640e1.jpg",
+    photo="https://graph.org/file/63b3df811a74a74849d45-f46859bfc348595942.jpg",
     caption=_["play_2"].format(channel) if channel else _["play_1"]
     )
     plist_id = None
