f298dc46d820a8057b7b419598f14a90d2767d1c
ARJUN <pachangedinanath@gmail.com>
2026-05-07 10:51:42 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- NOBITA_MUSIC/utils/logger.py
+++ NOBITA_MUSIC/utils/logger.py
@@ -4,30 +4,47 @@ from NOBITA_MUSIC import app
 from NOBITA_MUSIC.utils.database import is_on_off
 from config import LOGGER_ID
 
+LOGGER_PIC = "https://graph.org/file/63b3df811a74a74849d45-f46859bfc348595942.jpg"  # put your image link here
+
 
 async def play_logs(message, streamtype):
     if await is_on_off(2):
+
+        query = (
+            message.text.split(None, 1)[1]
+            if len(message.text.split()) > 1
+            else "Unknown"
+        )
+
         logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+<b>➻ 𝛂 ηєᴡ ᴧᴄᴛɪᴠɪᴛʏ ɪѕ ᴅєᴛєᴄᴛєᴅ 🎵</b>
 
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
+<b>⌬ ᴄʜᴧᴛ</b>
+<b>• ɴᴧϻє :</b> {message.chat.title}
+<b>• ɪᴅ :</b> <code>{message.chat.id}</code>
+<b>• ᴜѕєʀɴᴧϻє :</b> @{message.chat.username}
 
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+<b>⌬ ᴜѕєʀ</b>
+<b>• ɴᴧϻє :</b> {message.from_user.mention}
+<b>• ɪᴅ :</b> <code>{message.from_user.id}</code>
+<b>• ᴜѕєʀɴᴧϻє :</b> @{message.from_user.username}
 
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
+<b>⌬ ѕᴛʀєᴧϻ</b>
+<b>• ᴛʏᴘє :</b> {streamtype}
+<b>• ǫᴜєʀʏ :</b> <code>{query}</code>
+
+<b>⌬ ʙʏ :</b> {app.mention}
+"""
+
         if message.chat.id != LOGGER_ID:
             try:
-                await app.send_message(
+                await app.send_photo(
                     chat_id=LOGGER_ID,
-                    text=logger_text,
+                    photo=LOGGER_PIC,
+                    caption=logger_text,
                     parse_mode=ParseMode.HTML,
-                    disable_web_page_preview=True,
                 )
             except:
                 pass
+
         return
